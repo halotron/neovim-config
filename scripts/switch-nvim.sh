191bdd2f7b76e975df43ffae6812ba6b60991296
Jonas Jakobsson <jonas@knacka.se>
2025-11-19 16:59:30 +0100

    working script changer

---
+++ scripts/switch-nvim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# nvim-switch
+
+TARGET="$1"
+
+case $TARGET in
+    "lazy")
+        ln -sf ~/configvim/configlazy/nvim-lazy ~/.config/nvim
+        echo "Switched to LazyVim"
+        ;;
+    "original")
+        ln -sf ~/configvim/configstandard/nvim-standard ~/.config/nvim
+        echo "Switched to original config"
+        ;;
+    *)
+
+        ln -sf ~/configvim/configlazy/nvim-lazy ~/.config/nvim
+        echo "Switched to LazyVim"
+        ;;
+esac
+
